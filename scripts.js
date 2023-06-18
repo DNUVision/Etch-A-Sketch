764ae56cba55ae8ee379408fd91ccbc8c0c4d65c
DNUVision <davidfischsohn@gmail.com>
2023-06-18 22:42:13 +0300

    randomizing block color and comments explaining code

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
+// choosing the elements to manipulate, using regex to get dimensions for further grid calculations
 const creator = document.querySelector(".container");
 let containerWidth = getComputedStyle(creator);
 containerWidth = containerWidth.width;
 let re = /\d/g;
 let finalWidth = containerWidth.match(re);
 finalWidth = Number(finalWidth.join(""));
-console.log(finalWidth);
+// console.log(finalWidth); console debuger commented out 
 
+
+// initial grid dimensions setup
 function initialGrid(){
     const gridLayout = 256;
     const gridLayoutUnit = Math.sqrt(gridLayout);
@@ -20,6 +23,7 @@ function initialGrid(){
 
 initialGrid();
 
+// selecting the button and envoking a function that will delete the initial grid and create a new grid based on user input
 const btnSelector = document.querySelector("button");
 btnSelector.addEventListener("click", btnGrid);
 
@@ -35,7 +39,7 @@ function btnGrid(){
         creator.removeChild(divItem);
     });
     let limitGrid = limit ** 2;
-    console.log(limitGrid);
+    // console.log(limitGrid); console debuger commented out
     let rgbHolder = [];
     for(let block = 0; block < limitGrid; block++){
         const newDiv = document.createElement("div");
@@ -44,10 +48,24 @@ function btnGrid(){
         newDiv.style.width = ((finalWidth/limit) - 10) + "px";
         creator.appendChild(newDiv);
 
+
+}
+
+//randomizing block color by using built-in Math methods and passing them as rgb values
+const divSelector = document.querySelectorAll(".contained");
+divSelector.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+        let rgbHolder = [];
         for(let i = 0; i < 3; i++){
             let coloring = Math.floor(Math.random() * 256);
             rgbHolder.push(coloring);
         }
-        newDiv.style.backgroundColor = `rgb(${rgbHolder[0]},${rgbHolder[1]},${rgbHolder[2]})`;
-    }
+        div.style.backgroundColor = `rgb(${rgbHolder[0]},${rgbHolder[1]},${rgbHolder[2]})`;
+
+        })
+    });
 }
+
+    
+
+
